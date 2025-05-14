40767f80f06746af0cbad3dde6023d704d17fb00
miqueias199808@outlook.com <miqueias199808@outlook.com>
2025-05-14 18:53:52 +0200

    refac last test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sigueAI/',  
+  base: '/SigueAI/', 
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
